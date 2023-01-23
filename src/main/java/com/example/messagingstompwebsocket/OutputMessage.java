62968f0a60984a2448b76d33567d25391e6d8406
Mark Rybnikov <mark.rybnikov@amdaris.com>
2023-01-23 04:58:00 +0200

    Initial commit

---
+++ src/main/java/com/example/messagingstompwebsocket/OutputMessage.java
@@ -0,0 +1,13 @@
+package com.example.messagingstompwebsocket;
+
+public class OutputMessage {
+    private String from;
+    private String text;
+    private String time;
+
+    public OutputMessage(String from, String text, String time) {
+        this.from = from;
+        this.text = text;
+        this.time = time;
+    }
+}
